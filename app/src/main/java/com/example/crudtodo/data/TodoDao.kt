a393587b251962161632c9d6e7c3ae7bbe64f076
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-12-12 14:11:08 -0600

    add insertTodo fun to Dao

--- app/src/main/java/com/example/crudtodo/data/TodoDao.kt
+++ app/src/main/java/com/example/crudtodo/data/TodoDao.kt
@@ -1,9 +1,13 @@
 package com.example.crudtodo.data
 
+import androidx.room.Insert
 import androidx.room.Query
 
 interface TodoDao {
 
     @Query("Select * FROM todos")
     fun getTodos(): List<TodoItem>
+
+    @Insert
+    suspend fun insertTodo(todoItem: TodoItem)
 }
