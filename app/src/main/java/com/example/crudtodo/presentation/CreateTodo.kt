4178470d413d154c030d1699c0bb4e3280af4bc8
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-12-12 16:03:22 -0600

    add CreateTodo and TODOLists screens

---
+++ app/src/main/java/com/example/crudtodo/presentation/CreateTodo.kt
@@ -0,0 +1,55 @@
+package com.example.crudtodo.presentation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun CreateTodo(modifier: Modifier = Modifier, viewModel: TodoViewModel = hiltViewModel()) {
+    val titleState = viewModel.todoItemTitle.value
+
+
+    Scaffold(
+        bottomBar = { BottomAppBar {
+            Text(text = "Create Todo",modifier = Modifier.clickable {  })
+            Text(text = "TODO lists", modifier = Modifier.clickable {  })
+        }}
+    ) { paddingValues ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        ){
+            TextField(value = titleState, onValueChange = {viewModel.addTitle(it)})
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = { viewModel.addTitle(titleState) }) {
+                Text(text = "Add Todo")
+            }
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/crudtodo/presentation/TODOLists.kt
@@ -0,0 +1,79 @@
+package com.example.crudtodo.presentation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun TODOListsScreen(modifier: Modifier = Modifier, viewModel: TodoViewModel = hiltViewModel()) {
+
+    val localTodos = viewModel.localTodos.collectAsState().value
+    val remoteTodos = viewModel.remoteTodos.collectAsState().value
+
+    Scaffold(
+        bottomBar = { BottomAppBar {
+            Text(text = "Create Todo",modifier = Modifier.clickable {  })
+            Text(text = "TODO lists", modifier = Modifier.clickable {  })
+        }
+        }
+    ) { paddingValues ->
+
+        Row (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+
+        ){
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(localTodos) { todo ->
+                    Card {
+                        Row {
+                            Text(text = todo.title, modifier = Modifier.weight(1f))
+                            Text(text = "${todo.completed}", modifier = Modifier.weight(1f))
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(remoteTodos) { todo ->
+                    Card {
+                        Row {
+                            Text(text = todo.title, modifier = Modifier.weight(1f))
+                            Text(text = "${todo.completed}", modifier = Modifier.weight(1f))
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+
+            }
+        }
+    }
+}
