3db51202674ee6702af34ff4219a99cbbed1d80a
JorgeAndreTenorio <jorgeandretenorio@gmail.com>
2024-12-12 22:43:55 -0600

    make getLocalTodos function a suspend function so it doesn't block main thread

--- app/src/main/java/com/example/crudtodo/domain/repository/TodoRepository.kt
+++ app/src/main/java/com/example/crudtodo/domain/repository/TodoRepository.kt
@@ -4,7 +4,7 @@ import com.example.crudtodo.data.local.TodoItem
 
 interface TodoRepository {
 
-    fun getLocalTodos(): List<TodoItem>
+    suspend fun getLocalTodos(): List<TodoItem>
 
     suspend fun insertTodo(todoItem: TodoItem)
 
